4b7bd530075315e19df1e5e2771d79316d305d18
Autentyk <162135@student.uwm.edu.pl>
2023-03-06 15:44:41 +0100

    commit

--- Laboratoria 2/1_4_11/main.c
+++ Laboratoria 2/1_4_11/main.c
@@ -5,7 +5,7 @@ int main()
 {
     int a,b,c,d;
     scanf("%d %d %d %d",&a,&b,&c,&d);
-
+    //a
     int i =0;
     while(abs(a)*i*i+b*i+c<d){
         i++;
@@ -14,13 +14,13 @@ int main()
 
     for(int q =0;abs(a)*q*q+b*q+c<d;q++)
     printf("%d\n",q);
-
+    //b
     int j = 0;
     while(5*j*j+a*j+b<c){
         j++;
     }
     printf("%d\n",j);
-
+    //c
     int k = 0;
     while(5*k*k+a*k+b<=c){
         k++;
