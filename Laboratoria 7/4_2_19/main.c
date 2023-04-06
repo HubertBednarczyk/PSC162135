94555c233370fd3dd991b465d2d5141655985ffc
Autentyk <162135@student.uwm.edu.pl>
2023-04-06 15:22:59 +0200

    lab7

--- Laboratoria 7/4_2_19/main.c
+++ Laboratoria 7/4_2_19/main.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* copy_nonzero_elements(int n, int tab1[]) {
-    int nonzeros = 0;
+int* kopia_niezerowe(int n, int tab1[]) {
+    int rozne_od_zera = 0;
     for (int i = 0; i < n; i++) {
         if (tab1[i] != 0) {
-            nonzeros++;
+            rozne_od_zera++;
         }
     }
-    int* tab2 = malloc(nonzeros * sizeof(int));
+    int* tab2 = malloc(rozne_od_zera * sizeof(int));
     if (tab2 == NULL) {
         printf("Nie uda³o siê zaalokowaæ pamiêci dla tablicy tab2\n");
         exit(1);
@@ -30,7 +30,7 @@ int main() {
     int tab1[] = {1, 0, 2, 0, 3};
     int n = sizeof(tab1) / sizeof(tab1[0]);
 
-    int* tab2 = copy_nonzero_elements(n, tab1);
+    int* tab2 = kopia_niezerowe(n, tab1);
 
     printf("tab1: ");
     for (int i = 0; i < n; i++) {
