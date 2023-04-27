0c58b6a18e580717154a35cc9fb9560641283cc0
Autentyk <162135@student.uwm.edu.pl>
2023-04-27 16:06:38 +0200

    lab10

--- Laboratoria 10/6_2_12/main.c
+++ Laboratoria 10/6_2_12/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum_2d_array(int arr[][100], int n, int m) {
+int sum_2d_array(int **arr, int n, int m) {
     int sum = 0;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
@@ -11,7 +11,7 @@ int sum_2d_array(int arr[][100], int n, int m) {
     return sum;
 }
 
-void wyswietl(int n, int m, int tab[][m]){
+void wyswietl(int n, int m, int **tab){
     for(int i=0;i<n;i++){
         for(int j =0;j<m;j++){
             printf("[%d,%d] = %d ",i,j,tab[i][j]);
@@ -22,10 +22,25 @@ void wyswietl(int n, int m, int tab[][m]){
 }
 
 int main() {
-    int arr[3][100] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
     int n = 3;
     int m = 4;
-    int sum = sum_2d_array(arr, n, m);
-    printf("Suma: %d\n", sum);
+    int **tab1 = malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        tab1[i] = malloc(m * sizeof(int));
+    }
+    tab1[0][0] = 4;
+    tab1[0][1] = 5;
+    tab1[0][2] = 6;
+    tab1[0][3] = 7;
+    tab1[1][0] = 8;
+    tab1[1][1] = 9;
+    tab1[1][2] = 10;
+    tab1[1][3] = 11;
+    tab1[2][0] = 12;
+    tab1[2][1] = 13;
+    tab1[2][2] = 14;
+    tab1[2][3] = 15;
+
+    printf("suma el. : %d\n",sum_2d_array(tab1,n,m));
     return 0;
 }
